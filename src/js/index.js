a7878f317da037eeb9c1f7907750a6b42cf49dcc
liebner12 <liebner15@gmail.com>
2021-02-15 14:39:06 +0100

    Created popup window, with custom checkbox.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 /* Here goes your JS code */
+
+let popup = document.getElementsByClassName("popup");
+let openPopup = document.getElementById("show-popup-form");
+let closePopup = document.getElementById("close-popup-form");
+
+openPopup.addEventListener("click", () => {
+  popup[0].classList.toggle("active");
+});
+
+closePopup.addEventListener("click", () => {
+  popup[0].classList.toggle("active");
+});
